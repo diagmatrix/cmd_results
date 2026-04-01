89d9c4ce98641e815337e9ecd9d38a4e7a7f760d
diagmatrix <manuelgbal1@gmail.com>
2026-04-01 19:07:29 +0200

    Add partners pair

---
+++ db/tables/partners.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS public.partners (
+    first_id UUID NOT NULL REFERENCES public.cards(id),
+    second_id UUID NOT NULL REFERENCES public.cards(id),
+    name TEXT NOT NULL,
+    color_identity TEXT NOT NULL,
+    partner_type TEXT NOT NULL,
+    image_uri TEXT[] NOT NULL,
+    _created_at TIMESTAMP DEFAULT NOW(),
+	_updated_at TIMESTAMP,
+    PRIMARY KEY (first_id, second_id)
+);
+
+DROP TRIGGER IF EXISTS set_timestamp ON public.partners;
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON public.partners
+FOR EACH ROW EXECUTE FUNCTION public.trigger_set_timestamp();
+
+
+ALTER TABLE public.partners ENABLE ROW LEVEL SECURITY;
+CREATE POLICY "Anyone can read games" ON public.partners FOR SELECT USING (true);
+CREATE POLICY "Service role can insert partners" ON public.partners FOR INSERT TO service_role WITH CHECK (true);
+CREATE POLICY "Service role can update partners" ON public.partners FOR UPDATE TO service_role USING (true) WITH CHECK (true);
--- db/views/available_commanders.sql
+++ db/views/available_commanders.sql
@@ -1,15 +1,20 @@
 DROP VIEW IF EXISTS public.available_commanders;
 CREATE OR REPLACE VIEW public.available_commanders WITH(security_invoker = on) AS
 SELECT DISTINCT ON (name)
- name,
- set_code,
- coalesce(nullif(array_to_string(color_identity, ''), ''), 'C') AS color_identity,
- CASE
-    WHEN image_uris IS NOT NULL
-        THEN image_uris ->> 'large'
-    ELSE
-        (coalesce(raw_card ->> 'card_faces', '[]')::jsonb -> 1 ->> 'image_uris')::jsonb ->> 'large'
-END AS image_uri
+    name,
+    set_code,
+    coalesce(nullif(array_to_string(color_identity, ''), ''), 'C') AS color_identity,
+    CASE
+        WHEN image_uris IS NOT NULL
+            THEN image_uris ->> 'large'
+        ELSE
+            (coalesce(raw_card ->> 'card_faces', '[]')::jsonb -> 1 ->> 'image_uris')::jsonb ->> 'large'
+    END AS image_uri,
+    raw_card ->> 'oracle_text' AS oracle_text,
+    raw_card ->> 'keywords' AS keywords,
+    id,
+    type_line
 FROM public.cards
-WHERE set_code IS DISTINCT FROM 'sld'
+WHERE
+    set_code NOT IN ('sld', 'unk', 'mb1', 'mb2')
 ORDER BY name, released_at ASC NULLS LAST, collector_number ASC;
